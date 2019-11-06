13a1aa6b0ef577d27daa893aa0fe23fc0566f671
Antonio-Jung <antonio_jung@wamazing.jp>
2019-11-06 16:56:49 +0900

    make simple props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+// function Food(props) {
+//   console.log(props.name);
+//   return <h3>I love Poteto!</h3>
+// }
+
+/* sample thing*/
+function Food( {name} ){
+  return <h3>I love {name}</h3>
+}
+
+
 function App() {
   return (
-   <div>hello!</div>
+    <div>
+      <h1>Hello!</h1>
+      <Food name="Pizza" />
+      <Food name="Noodle" />
+      <Food name="Rice" />
+    </div>
   );
 }
 
