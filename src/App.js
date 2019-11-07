b4723a21d521ff8ea8d3c7be2d980cb48370b929
Antonio-Jung <antonio_jung@wamazing.jp>
2019-11-07 14:56:24 +0900

    install axios and use async and await functions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component{
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
 
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false })
-    }, 6000);
+  // async componentDidMount(){
+  //   const movies = axios.get("https://yts-proxy.now.sh/list_movies.json");
+  // }
+
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   }
+  componentDidMount(){
+    this.getMovies();
+  }
+
 
   render(){
     // use es6
